221f878ebeb61f57d539d9c735c1805311f5dc5e
Arsh Ansari <extinct_000-kaish@protonmail.com>
2026-08-14 12:57:00 +0530

    Update: Neovim config

--- AppData/Local/nvim-lab/init.lua
+++ AppData/Local/nvim-lab/init.lua
@@ -226,7 +226,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
 	callback = function()
-		vim.highlight.on_yank()
+		vim.hl.on_yank()
 	end,
 })
 -- Launch the Treesitter only when we have the required treesitter installed
