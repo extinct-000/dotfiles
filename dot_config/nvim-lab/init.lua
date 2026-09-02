0b73561eb747dfdf1ff2aa13647384fc4b9a10ef
Kaish Ansari <extinct_000-kaish@protonmail.com>
2026-09-02 10:28:14 +0530

    Add : new keymaps

--- dot_config/nvim-lab/init.lua
+++ dot_config/nvim-lab/init.lua
@@ -198,6 +198,10 @@ vim.opt.clipboard = "unnamedplus"
 
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
 vim.keymap.set("v", "<space>x", ":lua<CR>")
 vim.keymap.set("n", "<Tab>", ":bnext<CR>", { silent = true })
 vim.keymap.set("t", "<Esc>", [[<c-\><c-n>]])
