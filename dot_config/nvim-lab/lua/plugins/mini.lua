d640d48fe8fc8d1e3c72e6874ca9c57d67ec2180
Kaish Ansari <extinct_000-kaish@protonmail.com>
2026-09-21 12:12:16 +0530

    Remove : unnecessary code

--- dot_config/nvim-lab/lua/plugins/mini.lua
+++ dot_config/nvim-lab/lua/plugins/mini.lua
@@ -31,18 +31,6 @@ return {
 			require("mini.splitjoin").setup()
 			require("mini.cursorword").setup()
 			require("mini.statusline").setup({ use_icons = true })
-			require("mini.hipatterns").setup({
-				highlighters = {
-					-- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
-					fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
-					hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
-					todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
-					note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
-
-					-- Highlight hex color strings (`#rrggbb`) using that color
-					hex_color = require("mini.hipatterns").gen_highlighter.hex_color(),
-				},
-			})
 		end,
 	},
 }
