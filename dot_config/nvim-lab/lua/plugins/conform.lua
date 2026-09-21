d3a7aa535990a34235a5aadcb0c6384a6d1289a2
Kaish Ansari <extinct_000-kaish@protonmail.com>
2026-09-21 10:56:56 +0530

    Modified : Formatting of this file

--- dot_config/nvim-lab/lua/plugins/conform.lua
+++ dot_config/nvim-lab/lua/plugins/conform.lua
@@ -2,29 +2,20 @@ return {
 	"stevearc/conform.nvim",
 	config = function()
 		require("conform").setup({
-
 			formatters_by_ft = {
-
 				lua = { "stylua" },
 				python = { "ruff_format" },
 				javascript = { "prettier" },
 				html = { "prettier" },
 				json = { "prettier" },
 			},
-
 			formatters = {
 				stylua = {
-
 					prefer_local = true,
-					args = {
-						"--config-path",
-						vim.fn.expand("~/.config/stylua/stylua.toml")("-"),
-					},
+					args = { "--config-path", vim.fn.expand("~/.config/stylua/stylua.toml"), "-" },
 				},
 			},
-			default_format_opts = {
-				lsp_format = "never",
-			},
+			default_format_opts = { lsp_format = "never" },
 		})
 	end,
 }
